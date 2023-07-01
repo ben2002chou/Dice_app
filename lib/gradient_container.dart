bb9b24514730438550dc5ed5f02d6a30ec449db9
ben2002chou <114431877+ben2002chou@users.noreply.github.com>
2023-07-02 03:06:08 +0800

    add image

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,25 +1,25 @@
 import 'package:flutter/material.dart';
-import 'package:first_app/styled_text.dart';
+// import 'package:first_app/styled_text.dart';
 
 const startAlignment = Alignment.topLeft;
 const endAlignment = Alignment.bottomRight;
 
 class GradientContainer extends StatelessWidget {
-  const GradientContainer({super.key});
+  const GradientContainer(this.colors, {super.key});
+  final List<Color> colors;
   @override
   Widget build(context) {
     return Container(
-      decoration: const BoxDecoration(
+      decoration: BoxDecoration(
         gradient: LinearGradient(
-          colors: [
-            Colors.amber,
-            Colors.red,
-          ],
+          colors: colors,
           begin: startAlignment,
           end: endAlignment,
         ),
       ),
-      child: const Center(child: StyledText('Breathe Air!!!')),
+      child:  Center(
+        child: Image.asset('assets/images/dice-2.png'),
+      ),
     );
   }
 }
